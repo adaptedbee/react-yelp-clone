231d8dc93ac51225059f65395f5fd9c0a367f079
adaptedbee <adaptedbee@yandex.ru>
2017-02-13 00:15:39 +0300

    Подключение Font Awesome

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './app.css'
-
 import styles from './styles.module.css';
+import 'font-awesome/css/font-awesome.css';
 
 const App = React.createClass({
   render: function() {
     return (
       <div className={styles.wrapper}>
+        <i className="fa fa-star"></i>
         Text text text
       </div>
     );
