6796443aa8486e9175b792d5e95ad2dc3f749ad5
adaptedbee <adaptedbee@yandex.ru>
2017-02-26 17:23:26 +0300

    Getting a List of Places

--- src/views/main/Container.js
+++ src/views/main/Container.js
@@ -1,9 +1,19 @@
-import React, { PropTypes as T } from 'react'
+import React, { PropTypes as T } from 'react';
+import Map, {GoogleApiWrapper} from 'google-maps-react';
+import {searchNearby} from 'utils/googleApiHelpers';
 
-import Header from 'components/Header/Header'
-import styles from './styles.module.css'
+import Header from 'components/Header/Header';
+import styles from './styles.module.css';
 
 export class Container extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      places: [],
+      pagination: null
+    }
+  }
   renderChildren() {
     const childProps = {
       ...this.props
@@ -12,9 +22,38 @@ export class Container extends React.Component {
     return React.Children.map(children,
               c => React.cloneElement(c, childProps));
   }
+  onReady(mapProps, map) {
+    // When the map is ready and mounted
+    const {google} = this.props;
+    const opts = {
+      location: map.center,
+      radius: '500',
+      types: ['cafe']
+    };
+    searchNearby(google, map, opts)
+      .then((results, pagination) => {
+        // We got some results and a pagination object
+        this.setState({
+          places: results,
+          pagination
+        });
+      }).catch((status, result) => {
+        // There was an error
+      });
+  }
   render() {
     return (
       <div className={styles.wrapper}>
+        <Map
+          google={this.props.google}
+          onReady={this.onReady.bind(this)}
+          visible={false}>
+
+          {this.state.places.map(place => {
+            return (<div key={place.id}>{place.name}</div>)
+          })}
+
+        </Map>
         <Header tite="play" />
         <div className={styles.content}>
           {this.renderChildren()}
@@ -28,4 +67,6 @@ Container.contextTypes = {
   router: T.object
 }
 
-export default Container
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyASazw-8gBIeMf833apXVerbelOLWvMoa4'
+})(Container);
